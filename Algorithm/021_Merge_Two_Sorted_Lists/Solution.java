01704b17dc80ce992bc8ec2260a10cccaf37e8df
renchunxiao <rcx123tiancai@126.com>
2016-08-05 18:15:06 +0800

    add

---
+++ Algorithm/021_Merge_Two_Sorted_Lists/Solution.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode lastNode = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                lastNode.next = l1;
+                l1 = l1.next;
+            } else {
+                lastNode.next = l2;
+                l2 = l2.next;
+            }
+            lastNode = lastNode.next;
+        }
+        
+        if (l1 == null) {
+            lastNode.next = l2;   
+        }
+        
+        if (l2 == null) {
+            lastNode.next = l1;
+        }
+        
+        return dummy.next;
+    }
+    
+    /**
+     *  使用递归来merge，如果 l1 < l2,就拿l1.next 跟 l2 递归 merge 然后返回 l1，否则相反。
+     *
+     */
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2){
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
+		if(l1.val < l2.val){
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		} else{
+			l2.next = mergeTwoLists(l1, l2.next);
+			return l2;
+		}
+    }
+}
