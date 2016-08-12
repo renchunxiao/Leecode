18d1f7cc5a297d105767c439cd954d2a5b6079d4
renchunxiao <rcx123tiancai@126.com>
2016-08-12 17:23:12 +0800

    add

---
+++ Algorithm/053_Maximum_Subarray/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int local = nums[0];
+        int global = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            local = Math.max(nums[i], nums[i] + local);
+            global = Math.max(local, global);
+        }
+        
+        return global;
+    }
+}
