db43d9ba3292b1b650996bab6717fbf66463b2ca
renchunxiao <rcx123tiancai@126.com>
2016-08-16 22:29:35 +0800

    add

---
+++ Algorithm/203_Remove_Linked_List_Elements/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+	//用head指向当前节点的pre，然后利用next来判断是否相等
+        //然后移动head的位置，因为dummy的next指向head
+        //所以head也就是返回的开始位置
+        while (head.next != null) {
+            if (head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return dummy.next;
+    }
+}
