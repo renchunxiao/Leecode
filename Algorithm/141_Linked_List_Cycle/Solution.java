b090f7dd54d45e8cff0c2355aadc56d69480b8a3
renchunxiao <rcx123tiancai@126.com>
2016-08-16 17:08:47 +0800

    add

---
+++ Algorithm/141_Linked_List_Cycle/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        //一个一次走2步，一个一次走1步，如果有循环。那么一定会相等。
+        ListNode fast = head.next;
+        ListNode slow = head;
+        
+        while (fast != slow) {
+            if (fast.next == null || fast.next.next == null) {
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return true;
+    }
+}
