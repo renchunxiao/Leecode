a9de5a4b94e5831aed2a1a451e0a1e5ce578304e
renchunxiao <rcx123tiancai@126.com>
2016-08-17 17:30:06 +0800

    add

---
+++ Algorithm/102_Binary_Tree_Level_Order_Traversa/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        LinkedList<List<Integer>> ret = new LinkedList<List<Integer>>();
+        if (root == null) return ret;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            List<TreeNode> tmpNodes = new ArrayList<TreeNode>();
+            List<Integer> data = new ArrayList<Integer>();
+
+            int size = queue.size();
+            for(int i = 0; i <size; i++) {
+                TreeNode node = queue.remove();
+                data.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            ret.addLast(data);
+        }
+
+        return ret;
+    }
+}
