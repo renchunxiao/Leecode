64c9ad94d45beef6bb33536f364887aa61cecca1
renchunxiao <rcx123tiancai@126.com>
2016-08-19 17:54:42 +0800

    refactor

--- Algorithm/067_Add_Binary/Solution.java
+++ Algorithm/067_Add_Binary/Solution.java
@@ -12,18 +12,14 @@ public class Solution {
         String ret = "";
         int carry = 0;
         for (int i = al - 1, j = bl - 1; i >= 0; i--, j--) {
+	    int sum = 0;
             if (j < 0) {
-                char ac = a.charAt(i);
-                int sum = (int)(ac - '0') + carry;
-                ret = sum % 2 + ret;
-                carry = sum / 2;
+                sum = (a.charAt(i) - '0') + carry;
             } else {
-                char ac = a.charAt(i);
-                char bc = b.charAt(j);
-                int sum = (int)(ac - '0') + (int)(bc - '0') + carry;
-                ret = sum % 2 + ret;
-                carry = sum / 2;
+                sum = (a.charAt(i) - '0') + (b.charAt(j) - '0') + carry;
             }
+            ret = sum % 2 + ret;
+            carry = sum / 2;
         }
         if (carry == 1) {
             ret = "1" + ret;
