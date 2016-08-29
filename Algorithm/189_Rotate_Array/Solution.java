f01cf5f5eab0b86d76f83fcb3408daf1d63f9ac6
renchunxiao <rcx123tiancai@126.com>
2016-08-29 17:50:27 +0800

    add

---
+++ Algorithm/189_Rotate_Array/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    
+    private void reverse(int[] nums, int begin, int end) {
+        while (begin < end) {
+            int tmp = nums[begin];
+            nums[begin] = nums[end];
+            nums[end] = tmp;
+            begin++;
+            end--;
+        }
+    }
+}
